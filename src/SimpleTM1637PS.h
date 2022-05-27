c068e463e1a9b6a6c9c0820cba9d7bb13895c582
Piotr Sylwesiuk <47433781+veryblackcat@users.noreply.github.com>
2022-05-27 05:45:09 +0200

    Update SimpleTM1637PS.h

--- src/SimpleTM1637PS.h
+++ src/SimpleTM1637PS.h
@@ -43,12 +43,6 @@ const uint8_t digit2segments[] = {
   SEG_G | SEG_F | SEG_A | SEG_B | SEG_C | SEG_D,          // 9
   };
 
-//#define TM1637_CLK_LOW     pinMode(clk, OUTPUT)
-//#define TM1637_CLK_HIGH    pinMode(clk, INPUT)
-//#define TM1637_DIO_LOW     pinMode(dio, OUTPUT)
-//#define TM1637_DIO_HIGH    pinMode(dio, INPUT)
-//#define TM1637_CLK_WAITING delayMicroseconds(delayBUS)
-
 #define TM1637_DELAY_BUS   100
 #define TM1637_ACK_TIMEOUT 200
 
@@ -85,11 +79,11 @@ protected:
 	uint8_t sendByte(uint8_t data);
 	
 	// inline
-	void clkLOW()  {pinMode(clk, OUTPUT);} // #define TM1637_CLK_LOW     pinMode(clk, OUTPUT)
-	void clkHIGH() {pinMode(clk, INPUT); } // #define TM1637_CLK_HIGH    pinMode(clk, INPUT)
-	void dioLOW()  {pinMode(dio, OUTPUT);} // #define TM1637_DIO_LOW     pinMode(dio, OUTPUT)
-	void dioHIGH() {pinMode(dio, INPUT); } // #define TM1637_DIO_HIGH    pinMode(dio, INPUT)
-	void clkWaiting() {delayMicroseconds(delayBUS);} // #define TM1637_CLK_WAITING delayMicroseconds(delayBUS)
+	void clkLOW()  {pinMode(clk, OUTPUT);}
+	void clkHIGH() {pinMode(clk, INPUT); }
+	void dioLOW()  {pinMode(dio, OUTPUT);}
+	void dioHIGH() {pinMode(dio, INPUT); }
+	void clkWaiting() {delayMicroseconds(delayBUS);}
    
 private:
 	
